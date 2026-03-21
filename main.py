6a765564ed4ff6c4905e4a4c17cb058e90a0804e
Thrilok28021996 <emacs@localhost>
2026-03-21 20:53:24 +0530

    Basic rag

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import PyMuPDFLoader
-from langchain_core.vectorstores import InMemoryVectorStore
-from langchain_ollama import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 file_path = "/Users/thrilok/Documents/princeton_bitcoin_book.pdf"
@@ -9,21 +12,48 @@ documents = loader.load()
 embeddings = OllamaEmbeddings(
     model="nomic-embed-text:latest",
 )
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=150)
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=800, chunk_overlap=150, add_start_index=True
+)
 texts = text_splitter.split_documents(documents)
 # print("texts", texts)
 
+
 # Create a vector store with a sample text
-vectorstore = InMemoryVectorStore.from_documents(
-    texts,
-    embedding=embeddings,
+# FAISS vector store
+vectorstore = FAISS.from_documents(texts, embeddings)
+
+
+## initialize the model
+llm = ChatOllama(
+    model="llama3.2",  # or mistral / phi3 / gemma
+    temperature=0,
 )
 
-# Use the vectorstore as a retriever
-retriever = vectorstore.as_retriever()
+retriever = vectorstore.as_retriever(search_kwargs={"k": 4})  # top-4 relevant chunks
 
-retrieved_documents = retriever.invoke("What is bitcoin?")
 
-for i, doc in enumerate(retrieved_documents):
-    print(f"\n=== Result {i} ===")
-    print(doc.page_content.replace("\xa0", " ")[:500])
+## Define prompt
+prompt = ChatPromptTemplate.from_template("""
+You are a precise assistant. Answer ONLY using the context below.
+If the answer is not in the context, say "I don't know".
+
+Context:
+{context}
+
+Question:
+{input}
+""")
+
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+rag_chain = create_retrieval_chain(retriever, document_chain)
+
+
+response = rag_chain.invoke({"input": "What is Bitcoin mining?"})
+
+print(response["answer"])
+
+for doc in response["context"]:
+    print(doc.page_content[:201])
+    print("----")
